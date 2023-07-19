b085035e9761cd053c737f6f86b54f106efbefd4
manhtrancoder <manhtran050805@gmail.com>
2023-07-19 16:02:55 +0700

    Update window title and icon

--- src/Examine_Window.py
+++ src/Examine_Window.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtGui import QIcon
 from assets.ui.examine_window_ui import Ui_examine_window
 import assets.resources.resources_rc
 
@@ -8,3 +9,6 @@ class Examine_Window(QMainWindow):
         self.__ui = Ui_examine_window()
 
         self.__ui.setupUi(self)
+
+        self.setWindowTitle("Sát hạch lái xe A1")
+        self.setWindowIcon(QIcon(":icons/app_icon"))
