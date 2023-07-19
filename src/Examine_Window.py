3db65ead625e254f506afb7a70ce3ceb0e8fe9bd
manhtrancoder <manhtran050805@gmail.com>
2023-07-19 16:12:00 +0700

    Update settings for button

--- src/Examine_Window.py
+++ src/Examine_Window.py
@@ -10,10 +10,21 @@ class Examine_Window(QMainWindow):
 
         self.__ui.setupUi(self)
 
+        self.__setting_up_button()
+
         self.setWindowTitle("Sát hạch lái xe A1")
         self.setWindowIcon(QIcon(":icons/app_icon"))
+
+    def __setting_up_button(self):
+        self.__load_button_icon()
+        self.__connect_button_signal_and_slot()
 
     def __load_button_icon(self):
         self.__ui.next_question.setIcon(QIcon(":icons/next"))
         self.__ui.previous_question.setIcon(QIcon(":icons/previous"))
         self.__ui.submit.setIcon(QIcon(":icons/submit"))
+
+    def __connect_button_signal_and_slot(self):
+        self.__ui.next_question.clicked.connect(self.move_to_next_question)
+        self.__ui.previous_question.clicked.connect(self.move_to_previous_question)
+        self.__ui.submit.clicked.connect(self.submit)
